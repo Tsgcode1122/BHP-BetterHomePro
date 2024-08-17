b8f4c0a6552efc353ad481881b8dd259ee02db5e
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-08-17 23:15:25 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,114 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Gallery from "./Pages/Gallery";
+import Appointment from "./Pages/Appointment";
+import Contact from "./Pages/Contact";
+import { Result, Button } from "antd";
+import Layout from "./FixedComponent/Layout";
+import { Colors } from "./Colors/ColorComponent";
+import SingleService from "./Component/SingleService";
 
-function App() {
-  const [count, setCount] = useState(0)
+const GlobalStyle = createGlobalStyle`
+
+  html, body {
+    margin: 0 !important;
+    padding: 0 !important;
+  background: #f4faf1;
+ /* font-family: "Lora", system-ui; */
+
+    min-height: 100%;
+  }
+
+  body.modal-open {
+    overflow: hidden;
+  }
+
+  #root {
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+ 
+
+
+
+
+
+
+
+`;
+
+const StyledResult = styled(Result)`
+  .ant-result-title {
+    color: black !important;
+  }
+  .ant-result-subtitle {
+    color: black !important;
+  }
+`;
+
+// Component for handling invalid paths
+const InvalidPath = () => {
+  const navigate = useNavigate();
+
+  const handleBackHome = () => {
+    navigate("/");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <StyledResult
+      status="404"
+      title="404 Not Found"
+      subTitle="Oops! The page you are looking for does not exist."
+      extra={
+        <Button
+          type="primary"
+          onClick={handleBackHome}
+          style={{ background: "black" }}
+        >
+          Back Home
+        </Button>
+      }
+    />
+  );
+};
 
-export default App
+// Define your routes
+const routes = [
+  {
+    element: <Layout />,
+    children: [
+      { index: true, path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/appointment", element: <Appointment /> },
+
+      { path: "/services", element: <Services /> },
+      { path: "/gallery", element: <Gallery /> },
+      {
+        path: "/services/:serviceId",
+        element: <SingleService />,
+      },
+      { path: "*", element: <InvalidPath /> },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <RouterProvider router={router} />
+  </>
+);
+
+export default App;
