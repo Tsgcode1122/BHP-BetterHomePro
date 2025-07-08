d71aefc74fc0fde9080ac13c0cdd3352a6a77c39
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2025-07-08 19:47:50 -0400

    done

--- src/Component/QuickContact.jsx
+++ src/Component/QuickContact.jsx
@@ -109,7 +109,7 @@ const QuickContact = () => {
           <p>Get a Free Quote Now !!!</p>
           <ContactItem>
             <PhoneOutlined />
-            <a href="tel:+13072224021">704-360-8083</a>
+            <a href="tel:+17043608083">704-360-8083</a>
           </ContactItem>
           {/* Add other contact items as needed */}
         </Contact>
